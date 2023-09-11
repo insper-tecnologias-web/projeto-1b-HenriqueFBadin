47e070d124e055b9751ff68205c934ad11ad00ff
HenriqueFBadin <henriquefb3@al.insper.edu.br>
2023-09-11 18:04:04 -0300

    Handout 6 feito. Perguntar como aplicar o css e o script

---
+++ notes/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        titulo = self.title
+        id = self.id
+        return f'{id}.{titulo}'
